c0354eb73366d1cd04202b4b4325d9278da8c3c3
Ayush <ayushshende202@gmail.com>
2026-04-05 10:15:56 +0530

    modeified file in notebooks and in apps and also modified requirements.txt

--- apps/2_google_agent.py
+++ apps/2_google_agent.py
@@ -4,6 +4,7 @@ load_dotenv()
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import MemorySaver
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 search = GoogleSerperAPIWrapper()
@@ -11,13 +12,19 @@ search = GoogleSerperAPIWrapper()
 agent = create_agent(
     model=llm,
     tools=[search.run],
+    checkpointer=MemorySaver(),
     system_prompt="You are a agent who can search the web using google search"
 )
 
+quit_commands = {"exit", "quit", "bye", "goodbye", "see you later", "stop", "q"}
+
 while True:
     query = input("User:")
-    if query.lower() == "exit":
+    if query.strip().lower() in quit_commands:
         print("Agent: Goodbye!")
         break
-    res = agent.invoke({"messages":[{"role":"user","content":query}]})
+    res = agent.invoke(
+        {"messages":[{"role":"user","content":query}]},
+        {"configurable":{"thread_id":"google_agent_thread"}}
+        )
     print("Agent:" , res['messages'][-1].content)
